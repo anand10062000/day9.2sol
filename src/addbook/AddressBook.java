dcd55c751fa9a85f8ea01c7de5107a79012d1e3d
anand10062000 <anandkumarramisetti@gmail.com>
2021-09-25 09:55:05 +0530

    code for add data

--- src/addbook/AddressBook.java
+++ src/addbook/AddressBook.java
@@ -1,5 +1,45 @@
 package addbook;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import Addresbook.PersonInfo;
+
 public class AddressBook {
+	ArrayList<PersonInfo> persons;
+	Scanner sc = new Scanner(System.in);
+	
+	public AddressBook() {
+		persons = new ArrayList<PersonInfo>();
+	}
+
+	public void addPerson() {
+
+		System.out.println("Enter first name");
+		String firstname = sc.next();
+		
+		System.out.println("Enter last name");
+		String lastname = sc.next();
+		
+		System.out.println("Enter address");
+		String add = sc.next();
+		
+		System.out.println("Enter city name");
+		String city = sc.next();
+		
+		System.out.println("Enter state name");
+		String state = sc.next();
+		
+		System.out.println("Enter zip code");
+        int zip = sc.nextInt();
+
+		System.out.println("Enter phone no");
+		int phonenum = sc.nextInt();
+
+		PersonInfo p = new PersonInfo(firstname, lastname, add, city, state, zip, phonenum);
+
+		persons.add(p);
+	}
+
 
 }
--- src/addbook/Test.java
+++ src/addbook/Test.java
@@ -1,10 +1,26 @@
 package addbook;
 
+import java.util.Scanner;
+
 public class Test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		AddressBook add = new AddressBook();
+		add.toString();
+		Scanner sc1=new Scanner(System.in);
 
+
+		String name = null;
+
+		while(true){
+			System.out.println("\nEnter 1 to add\n Enter 2 to search\n Enter 3 to remove\n Enter 4 to update\n Enter 5 to display1");
+			int ch=sc1.nextInt(); //converting String to character
+			switch(ch){
+			case 1:
+				add.addPerson();
+				break;
+
+			}
+		}
 	}
-
 }
